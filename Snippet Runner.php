03793daefa6c3588817e1cedd8063858b0ce0e43
Siyck <me@siyck.com>
2011-11-01 22:39:32 +0100

    First full commit

---
+++ Snippet Runner.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<title>Snippet Runner</title>
+	
+	<meta charset="utf-8" />
+	<link rel="shortcut icon" href="./icon.png" />
+  
+	<script src="./mootools-core-1.4.1.js" language="JavaScript" type="text/javascript"></script>
+	<script src="./mootools-more-1.4.0.1.js" language="JavaScript" type="text/javascript"></script>
+
+	<script src="./<?php echo $_GET['q'] ?>" language="JavaScript" type="text/javascript"></script>
+</head>
+
+<body>
+  <div>
+    <header>
+      <h1>Snippet Runner</h1>
+    </header>
+  </div>
+</body>
+</html>
